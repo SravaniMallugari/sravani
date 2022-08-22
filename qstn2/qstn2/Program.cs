f41d9ea4551591b068de904f9444149dcff554cc
SravaniMallugari <111357901+SravaniMallugari@users.noreply.github.com>
2022-08-22 19:08:47 +0530

    Add files via upload

---
+++ qstn2/qstn2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qstn2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            Console.WriteLine(Convert.ToInt32("input enter first num1" + num1));
+            Console.WriteLine(Convert.ToInt32("input enter first num2" + num2));
+            Console.WriteLine(Convert.ToInt32("input enter first num3" + num3));
+            Console.WriteLine(Convert.ToInt32("output{0} * {1} *{2} "  num1 * num2 * num3));
+            Console.ReadLine();
+        }
+    }
+}
