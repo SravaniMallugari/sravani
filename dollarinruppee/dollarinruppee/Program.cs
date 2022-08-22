d88eb3b26fe40bd902011b743bdc5281ce8c14e6
SravaniMallugari <111357901+SravaniMallugari@users.noreply.github.com>
2022-08-22 19:16:45 +0530

    Add files via upload

---
+++ dollarinruppee/dollarinruppee/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dollarinruppee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dollar = 0;
+            double rupee = 0;
+            double value = 0;
+
+            Console.Write("Enter amount in dollar: ");
+            dollar = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the dollar value :");
+            value = double.Parse(Console.ReadLine());
+
+            rupee = dollar * value;
+            Console.WriteLine("dollar  " + dollar + "rupee " + rupee);
+            Console.ReadLine();
+        }
+    }
+}
