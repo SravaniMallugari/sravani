58fb45e574f1448c52ea883233483dbb70fb016b
SravaniMallugari <111357901+SravaniMallugari@users.noreply.github.com>
2022-08-29 21:41:43 +0530

    Add files via upload

---
+++ 29_handson/29_handson/qstn1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_handson
+{
+    class qstn1
+    {
+
+        static void Main(string[] args)
+        {
+            // string linked List
+            GenericsLinkedList<string> stringLinkedList = new GenericsLinkedList<string>(); //object 1
+            string str1 = "element1";
+            string str2 = "element2";
+            string str3 = "element3";
+            string str4 = "element4";
+            stringLinkedList.AddHead(str1);
+            stringLinkedList.AddHead(str2);
+            stringLinkedList.AddHead(str3);
+            stringLinkedList.AddHead(str4);
+            //display List
+            foreach (string s in stringLinkedList)
+            {
+                Console.WriteLine(s);
+            }
+
+            //Integer LinkedList
+            GenericsLinkedList<int> integerList = new GenericsLinkedList<int>();
+            int n1 = 10;
+            int n2 = 20;
+            int n3 = 30;
+
+            integerList.AddHead(n1);
+            integerList.AddHead(n2);
+            integerList.AddHead(n3);
+
+            foreach (int Intger in integerList)
+            {
+                Console.WriteLine(Intger);
+            }
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+
+
+    // Generic Linked List
+    class GenericsLinkedList<T>
+    {
+        class LinkedlistNode
+        {
+            private LinkedlistNode next;
+            private T item;
+
+            public LinkedlistNode(T t)
+            {
+                next = null;
+                item = t;
+
+            }
+            public LinkedlistNode Next
+            {
+                get
+                {
+                    return next;
+                }
+                set
+                {
+                    next = value;
+                }
+            }
+            public T Item
+            {
+                get
+                {
+                    return item;
+                }
+                set
+                {
+                    item = value;
+                }
+            }
+        }
+        private LinkedlistNode head;
+        public GenericsLinkedList()
+        {
+            head = null;
+        }
+        public void AddHead(T t)
+        {
+            LinkedlistNode node = new LinkedlistNode(t);
+            node.Next = head;
+            head = node;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            LinkedlistNode current = head;
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
+
+        }
+
+    }
+}
---
+++ 29_handson/29_handson/queue.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_handson
+{
+    internal class queue
+    {
+        static void Main(string[] args)
+        {
+            LinearQueue Q = new LinearQueue(5);
+
+            Q.insert(10);
+            Q.insert(20);
+            Q.insert(30);
+            Q.insert(40);
+            Q.insert(50);
+
+            Console.WriteLine("Items are : ");
+            Q.Queue();
+
+            Q.delete();
+            Q.delete();
+
+            Console.WriteLine("Items are : ");
+            Q.Queue();
+            Console.ReadLine();
+        }
+    }
+    class LinearQueue
+    {
+        private int[] ele;
+        private int front;
+        private int rear;
+        private int max;
+
+        public LinearQueue(int size)
+        {
+            ele = new int[size];
+            front = 0;
+            rear = -1;
+            max = size;
+        }
+
+        public void insert(int item)
+        {
+            if (rear == max - 1)
+            {
+                Console.WriteLine("Queue Overflow");
+                return;
+            }
+            else
+            {
+                ele[++rear] = item;
+            }
+        }
+
+        public int delete()
+        {
+            if (front == rear + 1)
+            {
+                Console.WriteLine("Queue is Empty");
+                return -1;
+            }
+            else
+            {
+                Console.WriteLine("deleted element is: " + ele[front]);
+                return ele[front++];
+            }
+        }
+
+        public void Queue()
+        {
+            if (front == rear + 1)
+            {
+                Console.WriteLine("Queue is Empty");
+                return;
+            }
+            else
+
+            {
+                for (int i = front; i <= rear; i++)
+                {
+                    Console.WriteLine("Item[" + (i + 1) + "]: " + ele[i]);
+                }
+            }
+        }
+
+    }
+}
