a42000521fe3a55dc741bce32de8ad2f8ab43a57
SravaniMallugari <111357901+SravaniMallugari@users.noreply.github.com>
2022-09-01 21:43:52 +0530

    Add files via upload

---
+++ 1_handson/1_handson/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_handson
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----Taken Array-----");
+            int[] MyArray = { 20, 5, 1000, 45, -2, 31, 27 };
+            
+            PrintArray(MyArray);
+
+            shellsort(MyArray);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("---Sorted Array----");
+            PrintArray(MyArray);
+            Console.ReadLine();
+        }
+        static void shellsort(int[] Array)
+            {
+                int n = Array.Length;
+                int gap = n / 2;
+                int temp, i, j;
+                while (gap > 0)
+                {
+                    for (i = gap; i < n; i++)
+                    {
+                        temp = Array[i];
+                        j = i;
+                        while (j >= gap && Array[j - gap] > temp)
+                        {
+                            Array[j] = Array[j - gap];
+                            j = j - gap;
+                        }
+                        Array[j] = temp;
+                    }
+                    gap = gap / 2;
+                }
+            }
+
+           
+            static void PrintArray(int[] Array)
+            {
+                int n = Array.Length;
+                for (int i = 0; i < n; i++)
+                    Console.Write(Array[i] + " ");
+                Console.Write("\n");
+            }
+
+            
+            
+        }
+    }
---
+++ 1_handson/1_handson/merge.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_handson
+{
+    internal class merge
+
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 20, 5, 1000, 45, -2, 31, 27 };
+            int len = numbers.Length;
+            Console.WriteLine("Before Merge Sorting :");
+            foreach (int item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("After Merge Sorting :");
+            Sorting(numbers, 0, len - 1);
+            foreach (int item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+            Console.Read();
+        }
+        static public void Mergesort(int[] numbers, int left, int mid, int right)
+        {
+            int[] temp = new int[25];
+            int i, left_end, num_elements, tmp_pos;
+            left_end = (mid - 1);
+            tmp_pos = left;
+            num_elements = (right - left + 1);
+            while ((left <= left_end) && (mid <= right))
+            {
+                if (numbers[left] <= numbers[mid])
+                    temp[tmp_pos++] = numbers[left++];
+                else
+                    temp[tmp_pos++] = numbers[mid++];
+            }
+            while (left <= left_end)
+                temp[tmp_pos++] = numbers[left++];
+            while (mid <= right)
+                temp[tmp_pos++] = numbers[mid++];
+            for (i = 0; i < num_elements; i++)
+            {
+                numbers[right] = temp[right];
+                right--;
+            }
+        }
+        static public void Sorting(int[] numbers, int left, int right)
+        {
+            int mid;
+            if (right > left)
+            {
+                mid = (right + left) / 2;
+                Sorting(numbers, left, mid);
+                Sorting(numbers, (mid + 1), right);
+                Mergesort(numbers, left, (mid + 1), right);
+            }
+        }
+      
+    }
+}
